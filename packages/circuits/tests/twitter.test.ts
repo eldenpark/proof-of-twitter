79738b0c11e781c9aeeb36fd958a7aab78cf0f2b
Elden Park <eldeniyenden@gmail.com>
2024-02-19 01:40:45 +0900

    o

--- packages/circuits/tests/twitter.test.ts
+++ packages/circuits/tests/twitter.test.ts
@@ -9,7 +9,7 @@ const F1Field = require("ffjavascript").F1Field;
 const Scalar = require("ffjavascript").Scalar;
 
 exports.p = Scalar.fromString(
-  "21888242871839275222246405745257275088548364400416034343698204186575808495617"
+  "21888242871839275222246405745257275088548364400416034343698204186575808495617",
 );
 
 describe("Twitter email test", function () {
@@ -17,24 +17,27 @@ describe("Twitter email test", function () {
 
   let dkimResult: import("@zk-email/helpers/src/dkim").DKIMVerificationResult;
   let circuit: any;
+  console.log("test start");
 
   beforeAll(async () => {
     const rawEmail = fs.readFileSync(
       path.join(__dirname, "./emls/test_twitter.eml"),
-      "utf8"
+      "utf8",
     );
     dkimResult = await verifyDKIMSignature(rawEmail);
-
     circuit = await wasm_tester(path.join(__dirname, "../twitter.circom"), {
       // NOTE: We are running tests against pre-compiled circuit in the below path
       // You need to manually compile when changes are made to circuit if `recompile` is set to `false`.
-      recompile: true,
+      // recompile: true,
+      recompile: false,
       output: path.join(__dirname, "../build/twitter"),
       include: path.join(__dirname, "../../../node_modules"),
     });
+    console.log("circuit compile done");
   });
 
   it("should verify twitter email", async function () {
+    console.log("verify start");
     const twitterVerifierInputs = generateTwitterVerifierCircuitInputs({
       rsaSignature: dkimResult.signature,
       rsaPublicKey: dkimResult.publicKey,
@@ -43,29 +46,35 @@ describe("Twitter email test", function () {
       message: dkimResult.message,
       ethereumAddress: "0x00000000000000000000",
     });
+
+    console.log(44, twitterVerifierInputs);
 
     const witness = await circuit.calculateWitness(twitterVerifierInputs);
     await circuit.checkConstraints(witness);
   });
 
-  it("should fail if the twitter_username_idx is invalid", async function () {
-    const twitterVerifierInputs = generateTwitterVerifierCircuitInputs({
-      rsaSignature: dkimResult.signature,
-      rsaPublicKey: dkimResult.publicKey,
-      body: dkimResult.body,
-      bodyHash: dkimResult.bodyHash,
-      message: dkimResult.message,
-      ethereumAddress: "0x00000000000000000000",
-    });
-    twitterVerifierInputs.twitter_username_idx = (Number(twitterVerifierInputs.twitter_username_idx) + 1).toString();
+  // it("should fail if the twitter_username_idx is invalid", async function () {
+  //   console.log("12333");
+  //   const twitterVerifierInputs = generateTwitterVerifierCircuitInputs({
+  //     rsaSignature: dkimResult.signature,
+  //     rsaPublicKey: dkimResult.publicKey,
+  //     body: dkimResult.body,
+  //     bodyHash: dkimResult.bodyHash,
+  //     message: dkimResult.message,
+  //     ethereumAddress: "0x00000000000000000000",
+  //   });
+  //   twitterVerifierInputs.twitter_username_idx = (
+  //     Number(twitterVerifierInputs.twitter_username_idx) + 1
+  //   ).toString();
 
-    expect.assertions(1);
-    try {
-      const witness = await circuit.calculateWitness(twitterVerifierInputs);
-      await circuit.checkConstraints(witness);
-    } catch (error) {
-      expect((error as Error).message).toMatch("Assert Failed");
-    }
+  //   console.log("twitterVeriferInputs", twitterVerifierInputs);
 
-  })
+  //   expect.assertions(1);
+  //   try {
+  //     const witness = await circuit.calculateWitness(twitterVerifierInputs);
+  //     await circuit.checkConstraints(witness);
+  //   } catch (error) {
+  //     expect((error as Error).message).toMatch("Assert Failed");
+  //   }
+  // });
 });
